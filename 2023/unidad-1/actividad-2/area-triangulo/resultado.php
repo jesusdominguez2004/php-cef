61b7abc712fd2750642049bbe0c8d22bfe7e047c
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2023-07-14 19:10:41 -0500

    CDN actividad 3 actualizados...

--- 2023/unidad-1/actividad-2/area-triangulo/resultado.php
+++ 2023/unidad-1/actividad-2/area-triangulo/resultado.php
@@ -39,5 +39,10 @@
         ?>
         <a href="index.html" class="btn btn-primary">Volver a index.html</a>
     </div>
+
+    <!-- JavaScript: jQuery, Bootstrap, pooperjs -->
+    <script src="https://code.jquery.com/jquery-3.7.0.slim.min.js" integrity="sha256-tG5mcZUtJsZvyKAxYLVXrmjKBVLd6VpVccqz/r4ypFE=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 </body>
 </html>
