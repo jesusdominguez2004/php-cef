fb48515bad06f4856113931ef754339ba8c6804c
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2023-07-28 20:04:10 -0500

    U2-A3 archivos iniciales...

---
+++ 2023/unidad-2/actividad-3/prueba-usuarios/clase_ingreso.php
@@ -0,0 +1,109 @@
+<?php
+use ClaseIngreso as GlobalClaseIngreso;
+    class ClaseIngreso {
+        private $codigo_u;
+        private $nombre_u;
+        private $apellido_u;
+        private $correo;
+
+        private $host;
+        private $user;
+        private $password;
+        private $database;
+        private $conexion;
+        
+        // métodos set y get
+        public function set_codigo_u($codigo_u) {
+            $this -> codigo_u = $codigo_u;
+        }
+
+        public function get_codigo_u() {
+            return $this -> codigo_u;
+        }
+
+        public function set_nombre_u($nombre_u) {
+            $this -> nombre_u = $nombre_u;
+        }
+
+        public function get_nombre_u() {
+            return $this -> nombre_u;
+        }
+
+        public function set_apellido_u($apellido_u) {
+            $this -> apellido_u = $apellido_u;
+        }
+
+        public function get_apellido_u() {
+            return $this -> apellido_u;
+        }
+
+        public function set_correo($correo) {
+            $this -> correo = $correo;
+        }
+
+        public function get_correo() {
+            return $this -> correo;
+        }
+
+        // constructor and new class Conexion (nuevos atributos)
+        public function __construct() {
+            $dato = new Conexion();
+            $this -> host = $dato -> get_host();
+            $this -> user = $dato -> get_user();
+            $this -> password = $dato -> get_password();
+            $this -> database = $dato -> get_database();
+        }
+
+        // 1. insertar usuario "prueba_tb_usuarios"
+        public function insertar_usuario(ClaseIngreso $usuario) {
+            try {
+                $this -> conexion = new PDO("mysql:host={$this->host};dbname={$this->database};charset=utf8", $this->user, $this->password);
+                $sql = "INSERT INTO prueba_tb_usuarios (nombre_u, apellido_u, correo) VALUES ('{$usuario->get_nombre_u()}', '{$usuario->get_apellido_u()}', '{$usuario->get_correo()}')";               
+                $iniciarSQL = $this -> conexion -> prepare($sql);
+                $iniciarSQL -> execute();
+            } catch (PDOException $ex) {
+                die ("ERROR INSERTAR USUARIO: {$ex->getMessage()}");
+            }
+        }
+
+        // 2. buscar usuario "prueba_tb_usuarios"
+        public function buscar_usuario(GlobalClaseIngreso $buscar) {
+            $matriz_usuarios = array();
+            try {
+                $this -> conexion = new PDO("mysql:host={$this->host};dbname={$this->database};charset=utf8", $this->user, $this->password);
+                $sql = "SELECT codigo_u, nombre_u, apellido_u, correo FROM prueba_tb_usuarios WHERE codigo_u = {$buscar->get_codigo_u()} ORDER BY apellido_u";              
+                $sth = $this -> conexion -> prepare($sql);
+                $sth -> execute();
+
+                while ($row = $sth -> fetch()) {
+                    $codigo_u = $row["codigo_u"];
+                    $nombre_u = $row["nombre_u"];
+                    $apellido_u = $row["apellido_u"];
+                    $correo = $row["correo"];
+                    $matriz_usuarios[] = array(
+                        "codigo_u" => $codigo_u, 
+                        "nombre_u" => $nombre_u, 
+                        "apellido_u" => $apellido_u, 
+                        "correo" => $correo
+                    );
+                }
+                $json_usuarios = json_encode($matriz_usuarios);
+                return $json_usuarios;
+            } catch (PDOException $ex) {
+                die ("ERROR BUSCAR USUARIO: {$ex->getMessage()}");
+            }
+        }
+
+        // 3. actualizar usuario "prueba_tb_usuarios"
+        public function actualizar_usuario(GlobalClaseIngreso $usuario) {
+            try {
+                $this -> conexion = new PDO("mysql:host={$this->host};dbname={$this->database};charset=utf8", $this->user, $this->password);
+                $sql = "UPDATE prueba_tb_usuarios SET nombre_u = '{$usuario->get_nombre_u()}', apellido_u = '{$usuario->get_apellido_u()}', correo = '{$usuario->get_correo()}' WHERE codigo_u = {$usuario->get_codigo_u()}";              
+                $sth = $this -> conexion -> prepare($sql);
+                $sth -> execute();
+            } catch (PDOException $ex) {
+                die ("ERROR ACTUALIZAR USUARIO: {$ex->getMessage()}");
+            }
+        }
+    }
+?>
---
+++ 2023/unidad-2/actividad-3/prueba-usuarios/conexion.php
@@ -0,0 +1,65 @@
+<?php
+    // Clase de conexión a bd_almacen
+    class Conexion {
+        private $host;
+        private $user;
+        private $password;
+        private $database;
+        private $conexion;
+
+        public function __construct() {
+            $this -> host = "localhost";
+            $this -> user = "root";
+            $this -> password = "";
+            $this -> database = "bd_almacen";
+        }
+
+        public function conectar() {
+            try {
+                $this -> conexion = new PDO(
+                    "mysql:host=" . $this -> host . ";dbname=" . $this -> database,
+                    $this -> user,
+                    $this -> password
+                );
+                echo "¡Conexión exitosa!";
+            } catch (PDOException $ex) {
+                echo "Problema de conexión: " . $ex -> getMessage();
+                exit;
+            }
+        }
+
+        // métodos get
+        public function get_host() {
+            return $this -> host;
+        }
+
+        public function get_user() {
+            return $this -> user;
+        }
+
+        public function get_database() {
+            return $this -> database;
+        }
+
+        public function get_password() {
+            return $this -> password;
+        }
+
+        // métodos set
+        public function set_host($host) {
+            $this -> host = $host;
+        }
+
+        public function set_user($user) {
+            $this -> user = $user;
+        }
+
+        public function set_database($database) {
+            $this -> database = $database;
+        }
+
+        public function set_password($password) {
+            $this -> password = $password;
+        }
+    }
+?>
